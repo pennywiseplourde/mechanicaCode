e39fc3f6e98fbd0275ceb423ab65173e7def7a35
Parker Plourde <theparkerman8@gmail.com>
2026-09-22 11:02:40 -0400

    added comments

--- robob/src/main.cpp
+++ robob/src/main.cpp
@@ -75,48 +75,36 @@ void autonomous() {}
  * task, not resume it from where it left off.
  */
 void opcontrol() {
-	pros::Controller master(pros::E_CONTROLLER_MASTER);
-	// pros::MotorGroup left_mg({1, -2, 3});    // Creates a motor group with forwards ports 1 & 3 and reversed port 2
-	// pros::MotorGroup right_mg({-4, 5, -6});  // Creates a motor group with forwards port 5 and reversed ports 4 & 6
+	pros::Controller master(pros::E_CONTROLLER_MASTER); // Makes the controller into an object called master, I think. This was automatically generated when I made the project.
 
-	int portFrontMotor;
-	int portBackMotor;
+	int portFrontMotor; // All of these need to be changed later to add the actual ports
+	int portBackMotor; // Integer means port, parity means forward(+)/reverse(-)
 	int starFrontMotor;
 	int starBackMotor;
 	int intakeMotor;
 	int portCascadeMotor;
 	int starCascadeMotor;
 
-	pros::MotorGroup portMG({portFrontMotor, portBackMotor});
-	pros::MotorGroup starMG({starFrontMotor, starBackMotor});
-	pros::MotorGroup intake({intakeMotor});
-	pros::MotorGroup cascade({portCascadeMotor, starCascadeMotor});
+	pros::MotorGroup portMG({portFrontMotor, portBackMotor}); // Creates the motor group for the port side
+	pros::MotorGroup starMG({starFrontMotor, starBackMotor}); // Creates the motor group for the star side
+	pros::MotorGroup intake({intakeMotor});	// Creates the motor group for the pin intake
+	pros::MotorGroup cascade({portCascadeMotor, starCascadeMotor}); // Creates the motor group for the cascade lift
 
 
-	while (false) {
-		pros::lcd::print(0, "%d %d %d", (pros::lcd::read_buttons() & LCD_BTN_LEFT) >> 2,
-		                 (pros::lcd::read_buttons() & LCD_BTN_CENTER) >> 1,
-		                 (pros::lcd::read_buttons() & LCD_BTN_RIGHT) >> 0);  // Prints status of the emulated screen LCDs
-
-		// Arcade control scheme
-		int dir = master.get_analog(ANALOG_LEFT_Y);    // Gets amount forward/backward from left joystick
-		int turn = master.get_analog(ANALOG_RIGHT_X);  // Gets the turn left/right from right joystick
-		left_mg.move(dir - turn);                      // Sets left motor voltage
-		right_mg.move(dir + turn);                     // Sets right motor voltage
-		pros::delay(20);                               // Run for 20 ms then update
-	}
-
-	while (true) {
+	while (true) { // Running loop, the code in these brackets will loop until the game ends
 		int dir = master.get_analog(ANALOG_LEFT_Y); // Gets the forward/back from the left joystick
 		int pivot = master.get_analog(ANALOG_RIGHT_X); //gets the left/right from the right joystick
 
-		pivot = std::sqrt(pivot);
-		pivot = pivot * 10;
+		pivot = std::sqrt(pivot); // These two lines apply the square root curve to the amount
+		pivot = pivot * 10;       // of pivot, which makes it feel more natural to the driver.
 
-		portMG.move(dir - pivot);
-		starMG.move(dir + pivot);
+		/* I use port and starboard (or star for short) when refering to the sides,
+		    because left and right can be ambiguous and confusing*/
 
-		pros::delay(20);
+		portMG.move(dir - pivot); // Powers the port motors, minus any amount of turning
+		starMG.move(dir + pivot); // Powers the starboard motors, sum any amunt of turning
+
+		pros::delay(20); // This marks the polling rate, this programs polls user input every 20ms
 
 	}
 }
